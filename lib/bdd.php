b3aaf70383389cbf3d46ba11a4145f84eb4b2ebc
romaincoeur <coeurro@gmail.com>
2013-12-07 23:43:16 +0100

    fonction d'upload

--- lib/bdd.php
+++
@@ -1,9 +0,0 @@
-<?php $link = mysql_connect("parent-nounou.fr", "innovedu_comptin", "Z%u[fI_iE1T-")
-    or die("Impossible de se connecter : " . mysql_error());
-//echo 'Connexion réussie';
-$db_selected = mysql_select_db("innovedu_comptines_DEV", $link);
-mysql_query("SET NAMES 'utf8'"); 
-if (!$db_selected) {
-   die ('Impossible de sélectionner la base de données : ' . mysql_error());
-}
-?>
