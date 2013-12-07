b81d4857ae0c0f0c75ec63f8ae1a02bed8c97533
romaincoeur <coeurro@gmail.com>
2013-12-07 11:48:04 +0100

    correction architecture mvc et web services

--- vues/v_bandeau.php
+++
@@ -1,7 +0,0 @@
-<div id="bandeau">
-	<h1> La boite a comptines</h1>
-	<ul id="menu">
-	<li><a href="index.php?uc=rechercher&action=afficher"> Rechercher</a></li>
-	<li><a href="index.php?uc=publier&action=afficher"> Publier</a></li>
-</ul>
-</div>
